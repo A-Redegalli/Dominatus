ab1259caebde86ff522f8598bdf22a60e3335ad2
AEndrix <redegalli.andrea03@gmail.com>
2025-04-08 02:32:34 +0200

    adattamento a librerie comuni

--- src/main/java/it/aredegalli/dominatus/DominatusApplication.java
+++ src/main/java/it/aredegalli/dominatus/DominatusApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "it.aredegalli")
 public class DominatusApplication {
 
     public static void main(String[] args) {
